bf0a097084912941f1a3e52d04004dfa9bfced40
lexach <lexachsar@gmail.com>
2018-02-17 03:33:47 +0400

    new file:   xfce/install.sh (installation script for XFCE DE)

---
+++ xfce/install.sh
@@ -0,0 +1,9 @@
+# Dependencies
+
+## Appearence
+
+### Install the arc-theme
+sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+apt-get -qq update
+apt-get -qq --assume-yes install arc-theme
+rm -f -v /etc/apt/sources.list.d/arc-theme.list
