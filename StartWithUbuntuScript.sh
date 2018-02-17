3e6237c332cd8475cbe4a596507e4d006f5c4e07
lexach <lexachsar@gmail.com>
2018-02-17 20:29:02 +0400

    modified:   StartWithUbuntuScript.sh

--- StartWithUbuntuScript.sh
+++ StartWithUbuntuScript.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
-apt-get install ubuntu-restricted-extras
+apt -qq --assume-yes update
+apt -qq --assume-yes upgrade
+apt install -qq --assume-yes ubuntu-restricted-extras
 
 #install microsoft fonts
 echo -en "\033[37;1;41m Microsoft Fonts \033[0m Installation \n"
-sudo apt install msttcorefonts
-sudo fc-cache -fv
+apt -qq --assume-yes install msttcorefonts
+fc-cache -fv
 
 #install telegram
 echo -en "\033[37;1;41m Telegram \033[0m Installation \n"
-sudo add-apt-repository ppa:atareao/telegram
-sudo apt-get update
-sudo apt-get install telegram
+add-apt-repository -y ppa:atareao/telegram
+apt -qq --assume-yes update
+apt -qq --assume-yes install telegram
 
 #install spotify
 echo -en "\033[37;1;41m Spotify \033[0m Installation \n"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
+apt update
+apt -qq --assume-yes install spotify-client
 
 #install ssh
 echo -en "\033[37;1;41m SSH \033[0m Installation \n"
-sudo apt-get install ssh
+apt -qq --assume-yes install ssh
 #install flash for firefox
 echo -en "\033[37;1;41m FlashPlayer(pepper flash) \033[0m (for Firefox) Installation \n"
-sudo apt-get install browser-plugin-freshplayer-pepperflash
+apt -qq --assume-yes install browser-plugin-freshplayer-pepperflash
 #install chrome
 echo -en "\033[37;1;41m Chromium \033[0m Installation \n"
-sudo apt-get install chromium-browser
+apt -qq --assume-yes install chromium-browser
 
 #install unetbootin
 echo -en "\033[37;1;41m Unetbootin \033[0m Installation \n"
-sudo add-apt-repository ppa:gezakovacs/ppa
-sudo apt-get update
-sudo apt-get install unetbootin
+add-apt-repository -y ppa:gezakovacs/ppa
+apt update
+apt -qq --assume-yes install unetbootin
 
 #change virtual workspaces (unity option)
 #echo -en "\033[37;1;41m Wirtual Workspaces Layout Changing (Ubuntu ONLY) \033[0m\n"
@@ -45,40 +45,40 @@ sudo apt-get install unetbootin
 
 #install intellij idea
 echo -en "\033[37;1;41m Intellij Idea \033[0m Installation \n"
-sudo add-apt-repository ppa:ubuntuhandbook1/apps
-sudo apt-get update
-sudo apt-get install intellij-idea-community
+add-apt-repository -y ppa:ubuntuhandbook1/apps
+apt-get update
+apt -qq --assume-yes install intellij-idea-community
 
 #install octave
 echo -en "\033[37;1;41m Octave \033[0m Installation \n"
-sudo apt-get install octave
+apt -qq --assume-yes install octave
 
 #install ru package for libreoffice
 echo -en "\033[37;1;41m LibreOffice RU package \033[0m Installation \n"
-sudo apt-get install libreoffice-l10n-ru
+apt -qq --assume-yes install libreoffice-l10n-ru
 
 #install vim
 echo -en "\033[37;1;41m Vim \033[0m Installation \n"
-sudo apt-get install vim
+apt -qq --assume-yes install vim
 
 #install g++
 echo -en "\033[37;1;41m GCC \033[0m Installation \n"
-sudo apt-get install g++
+apt -qq --assume-yes install g++
 
 #install tmux
 echo -en "\033[37;1;41m TMUX \033[0m Installation \n"
-sudo apt install tmux
+apt -qq --assume-yes install tmux
 
 #install nmap
 echo -en "\033[37;1;41m NMap \033[0m Installation \n"
-sudo apt install nmap
+apt -qq --assume-yes install nmap
 
 #install git
 echo -en "\033[37;1;41m Git \033[0m Installation \n"
-sudo apt install git
+apt -qq --assume-yes install git
 
 #install xclip for github ssh key add
-sudo apt install xclip
+apt -qq --assume-yes install xclip
 
 #install TrueCrypt
 echo -en "\033[37;1;41m True Crypt \033[0m Installation \n"
@@ -89,7 +89,7 @@ tar -xzvf truecrypt-7.1a-linux-console-x64.tar.gz
 
 #install CryptSetup
 echo -en "\033[37;1;41m Crypt Setup \033[0m Installation \n"
-apt install cryptsetup
+apt -qq --assume-yes install cryptsetup
 
 #install workflowy
 echo -en "\033[37;1;41m Workflowy \033[0m Installation \n"
@@ -100,12 +100,12 @@ rm -f -v Workflowy.AppImage
 
 #install curl
 echo -en "\033[37;1;41m Curl \033[0m Installation \n"
-sudo apt install curl
+apt -qq --assume-yes install curl
 
 #MySql installation
 echo -en "\033[37;1;41m MySql \033[0m Installation \n"
-sudo apt install mysql-server
+apt -qq --assume-yes install mysql-server
 
 #MySql-workbench installation
 echo -en "\033[37;1;41m MySql-Workbench \033[0m Installation \n"
-sudo apt install mysql-workbench
+apt -qq --assume-yes install mysql-workbench
