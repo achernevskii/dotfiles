88b38c1cdab3ef0c1d57f41fb2a4cc8dac03cf9d
Alex Chernevskiy <lexachsar@gmail.com>
2018-02-21 23:23:04 +0400

    modified:   StartWithUbuntuScript.sh (changed packet manager from apt to apt-get)

--- StartWithUbuntuScript.sh
+++ StartWithUbuntuScript.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-apt -qq --assume-yes update
-apt -qq --assume-yes upgrade
-apt install -qq --assume-yes ubuntu-restricted-extras
+apt-get -qq --assume-yes update
+apt-get -qq --assume-yes upgrade
+apt-get install -qq --assume-yes ubuntu-restricted-extras
 
 #install microsoft fonts
 echo -en "\033[37;1;41m Microsoft Fonts \033[0m Installation \n"
-apt -qq --assume-yes install msttcorefonts
+apt-get -qq --assume-yes install msttcorefonts
 fc-cache -fv
 
 #install telegram
 echo -en "\033[37;1;41m Telegram \033[0m Installation \n"
 add-apt-repository -y ppa:atareao/telegram
-apt -qq --assume-yes update
-apt -qq --assume-yes install telegram
+apt-get -qq --assume-yes update
+apt-get -qq --assume-yes install telegram
 
 #install spotify
 echo -en "\033[37;1;41m Spotify \033[0m Installation \n"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-apt update
-apt -qq --assume-yes install spotify-client
+sudo apt-get-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt-get/sources.list.d/spotify.list
+apt-get update
+apt-get -qq --assume-yes install spotify-client
 
 #install ssh
 echo -en "\033[37;1;41m SSH \033[0m Installation \n"
-apt -qq --assume-yes install ssh
+apt-get -qq --assume-yes install ssh
 #install flash for firefox
 echo -en "\033[37;1;41m FlashPlayer(pepper flash) \033[0m (for Firefox) Installation \n"
-apt -qq --assume-yes install browser-plugin-freshplayer-pepperflash
+apt-get -qq --assume-yes install browser-plugin-freshplayer-pepperflash
 #install chrome
 echo -en "\033[37;1;41m Chromium \033[0m Installation \n"
-apt -qq --assume-yes install chromium-browser
+apt-get -qq --assume-yes install chromium-browser
 
 #install unetbootin
 echo -en "\033[37;1;41m Unetbootin \033[0m Installation \n"
 add-apt-repository -y ppa:gezakovacs/ppa
-apt update
-apt -qq --assume-yes install unetbootin
+apt-get update
+apt-get -qq --assume-yes install unetbootin
 
 #change virtual workspaces (unity option)
 #echo -en "\033[37;1;41m Wirtual Workspaces Layout Changing (Ubuntu ONLY) \033[0m\n"
@@ -46,39 +46,39 @@ apt -qq --assume-yes install unetbootin
 #install intellij idea
 echo -en "\033[37;1;41m Intellij Idea \033[0m Installation \n"
 add-apt-repository -y ppa:ubuntuhandbook1/apps
-apt-get update
-apt -qq --assume-yes install intellij-idea-community
+apt-get-get update
+apt-get -qq --assume-yes install intellij-idea-community
 
 #install octave
 echo -en "\033[37;1;41m Octave \033[0m Installation \n"
-apt -qq --assume-yes install octave
+apt-get -qq --assume-yes install octave
 
 #install ru package for libreoffice
 echo -en "\033[37;1;41m LibreOffice RU package \033[0m Installation \n"
-apt -qq --assume-yes install libreoffice-l10n-ru
+apt-get -qq --assume-yes install libreoffice-l10n-ru
 
 #install vim
 echo -en "\033[37;1;41m Vim \033[0m Installation \n"
-apt -qq --assume-yes install vim
+apt-get -qq --assume-yes install vim
 
 #install g++
 echo -en "\033[37;1;41m GCC \033[0m Installation \n"
-apt -qq --assume-yes install g++
+apt-get -qq --assume-yes install g++
 
 #install tmux
 echo -en "\033[37;1;41m TMUX \033[0m Installation \n"
-apt -qq --assume-yes install tmux
+apt-get -qq --assume-yes install tmux
 
 #install nmap
 echo -en "\033[37;1;41m NMap \033[0m Installation \n"
-apt -qq --assume-yes install nmap
+apt-get -qq --assume-yes install nmap
 
 #install git
 echo -en "\033[37;1;41m Git \033[0m Installation \n"
-apt -qq --assume-yes install git
+apt-get -qq --assume-yes install git
 
 #install xclip for github ssh key add
-apt -qq --assume-yes install xclip
+apt-get -qq --assume-yes install xclip
 
 #install TrueCrypt
 echo -en "\033[37;1;41m True Crypt \033[0m Installation \n"
@@ -89,7 +89,7 @@ tar -xzvf truecrypt-7.1a-linux-console-x64.tar.gz
 
 #install CryptSetup
 echo -en "\033[37;1;41m Crypt Setup \033[0m Installation \n"
-apt -qq --assume-yes install cryptsetup
+apt-get -qq --assume-yes install cryptsetup
 
 #install workflowy
 echo -en "\033[37;1;41m Workflowy \033[0m Installation \n"
@@ -100,12 +100,12 @@ rm -f -v Workflowy.AppImage
 
 #install curl
 echo -en "\033[37;1;41m Curl \033[0m Installation \n"
-apt -qq --assume-yes install curl
+apt-get -qq --assume-yes install curl
 
 #MySql installation
 echo -en "\033[37;1;41m MySql \033[0m Installation \n"
-apt -qq --assume-yes install mysql-server
+apt-get -qq --assume-yes install mysql-server
 
 #MySql-workbench installation
 echo -en "\033[37;1;41m MySql-Workbench \033[0m Installation \n"
-apt -qq --assume-yes install mysql-workbench
+apt-get -qq --assume-yes install mysql-workbench
