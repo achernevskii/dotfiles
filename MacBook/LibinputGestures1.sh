480937b8e491b7fedaea81c3957071f3d545f897
Alex Chernevskiy <lexachsar@gmail.com>
2018-05-14 00:31:14 +0400

    Removed MacDook directory.

--- MacBook/LibinputGestures1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#You must be a member of the input group to have permission to read the touchpad device:
-sudo gpasswd -a lexach input
-reboot
--- MacBook/sleep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo modprobe -r bcm5974
-sudo modprobe bcm5974
-
-dbus-launch
-libinput-gestures-setup restart
