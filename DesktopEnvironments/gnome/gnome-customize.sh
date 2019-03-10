f3a915a127b24923a6d29a253da2c990dcc3c269
Alex Chernevskiy <lexachsar@gmail.com>
2019-03-10 22:50:38 +0400

    Changed power button action to hibernation.

--- DesktopEnvironments/gnome/gnome-customize.sh
+++ DesktopEnvironments/gnome/gnome-customize.sh
@@ -19,3 +19,6 @@ gsettings set org.gnome.shell.app-switcher current-workspace-only true
 
 # Use Alt + Left Shift to change input layout
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>Shift_L']"
+
+# Power button now hibernates laptop
+gsettings set org.gnome.settings-daemon.plugins.power power-button-action hibernate
