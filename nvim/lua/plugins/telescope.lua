46cebcc66196bd73150013147c2ad56e21fecd24
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2024-06-04 00:32:12 -0500

    Update telescope configuration
    * Add fzf sorter for telescope.
    
    * Add new key bindings for buffers, help tags, git branches and commits.
    
    * Add telescope-ui-select plugin to use telescope for vim.ui.select
      actions.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,27 +1,48 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.6',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		end
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					git_branches = {
+						theme = "dropdown",
+					},
+				},
+			})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+			-- Git pickers
+			vim.keymap.set("n", "<leader>gfb", builtin.git_branches, {})
+			vim.keymap.set("n", "<leader>gfc", builtin.git_commits, {})
+
+			-- To get fzf loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("fzf")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			-- This is your opts table
-			require("telescope").setup {
-			  extensions = {
-			    ["ui-select"] = {
-			      require("telescope.themes").get_dropdown {
-			      }
-			    }
-			  }
-			}
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 			require("telescope").load_extension("ui-select")
-		end
+		end,
 	},
 }
