96068369454f0e98cff073b6ebad9fad74eadf60
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2024-06-13 12:35:54 -0500

    Add diagnostic kymaps, centering after page up/down
    * Add keybindings for the diagnostic messages (jumping between messages,
      etc.).
    
    * Add centering (zz) after page up and page down movements (<C-u> and <C-d>).

--- nvim/lua/vim-config.lua
+++ nvim/lua/vim-config.lua
@@ -48,10 +48,10 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
---vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
---vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
---vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
---vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- TIP: Disable arrow keys in normal mode
 --vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -83,4 +83,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-
+-- Page up and down movements should center the page
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
