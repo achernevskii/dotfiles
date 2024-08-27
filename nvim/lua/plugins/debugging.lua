c053a92e8205b8336d91db8bf4009490ddb6f3d8
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2024-08-27 13:54:22 -0500

    Add python-dap adapter for the neovim config

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -4,14 +4,17 @@ return {
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
-			-- Golang support
+			-- Adapters
 			"leoluz/nvim-dap-go",
+			"mfussenegger/nvim-dap-python"
 		},
 		config = function()
 			local dap = require("dap")
-
 			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
 			vim.keymap.set("n", "<leader>dc", dap.continue, {})
+			vim.keymap.set("n", "<leader>do", dap.step_over, {})
+			vim.keymap.set("n", "<leader>di", dap.step_into, {})
+			vim.keymap.set("n", "<leader>doo", dap.step_out, {})
 
 			local dapui = require("dapui")
 
@@ -31,6 +34,7 @@ return {
 			dapui.setup()
 
 			require("dap-go").setup()
+			require("dap-python").setup("python")
 		end,
 	},
 }
