12d5647ef475c826240cb7bfdac37d5ad582ac32
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2024-08-28 00:32:20 -0500

    Update git and lualine settings for neovim

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,14 +3,16 @@ return {
 		"tpope/vim-fugitive",
 		config = function()
 			vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", {})
-			vim.keymap.set("n", "<leader>gl", ":Git log<CR>", {})
-		end
+		end,
 	},
-
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			})
 
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 			vim.keymap.set("n", "<leader>gtb", ":Gitsigns toggle_current_line_blame<CR>", {})
